4918f7274ad8ed81cc6fbc51463225e3ceff5093
tomegoodson <158308016+tomegoodson@users.noreply.github.com>
2024-04-02 09:38:35 -0500

    jsxlab.tg

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
-import React from "react";
-import { image } from "../data/data";
+// About.js
+import React from 'react';
+import { image } from '../data/data.js'; // Assuming you have imported 'image' from your data file
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Insert your content here...</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
-import React from "react";
-import { name, city } from "../data/data.js";
+// Home.js
+import React from 'react';
+import { name, city } from '../data/data.js'; // Assuming you have imported 'name' and 'city' from your data file
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 style={{ color: 'firebrick' }}>{name} is a Web Developer from {city}</h1>
+    </div>
+  );
 }
 
 export default Home;
